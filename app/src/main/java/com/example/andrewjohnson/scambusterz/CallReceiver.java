185193ba2a03820e908734ac82537f21bf8db8ac
Andrew Johnson <iglioflop1@gmail.com>
2018-11-10 22:39:38 -0500

    CallReceiver before merge

--- app/src/main/java/com/example/andrewjohnson/scambusterz/CallReceiver.java
+++ app/src/main/java/com/example/andrewjohnson/scambusterz/CallReceiver.java
@@ -1,18 +1,11 @@
 package com.example.andrewjohnson.scambusterz;
 
-import android.app.job.JobParameters;
-import android.app.job.JobService;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.AssetManager;
-import android.provider.ContactsContract;
 import android.telephony.PhoneStateListener;
-import android.telephony.TelephonyManager;
 
 public class CallReceiver extends PhoneStateListener {
     @Override
     public void onCallStateChanged(int state, String phoneNumber) {
+        System.out.println("Phone: " + phoneNumber);
         super.onCallStateChanged(state, phoneNumber);
     }
 }
