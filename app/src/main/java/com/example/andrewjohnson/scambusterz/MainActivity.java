1cfccde2baf471f32451c8d89ed6555f91b77c15
Daniel Visca <danielvisca96@gmail.com>
2018-11-11 09:13:13 -0500

    Parsed response!

--- app/src/main/java/com/example/andrewjohnson/scambusterz/MainActivity.java
+++ app/src/main/java/com/example/andrewjohnson/scambusterz/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.os.Build;
@@ -18,6 +17,24 @@ import okhttp3.Callback;
 import okhttp3.Response;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+
+// Parse JSON
+import org.json.JSONObject;
+import org.json.JSONArray;
+//import org.json.JSONException;
+import org.json.JSONException;
+import java.util.List;
+import java.util.ArrayList;
+import org.json.JSONObject;
+
+//import org.json.JSONArray;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//import org.json.JSONString;
+//import org.json.JSONStringer;
+//import org.json.simple.JSONObject;
+//import org.json.simple.parser.JSONParser;
+//import org.json.simple.parser.ParseException;
 
 public class MainActivity extends AppCompatActivity {
     private final String CHANNEL_ID = "Spam Caller";
@@ -74,17 +91,56 @@ public class MainActivity extends AppCompatActivity {
         String url = urlFirst + phoneNumber + urlLast;
         request = new Request.Builder().url(url).build();
 
-        String jsonResponse = "Nothing";
+        final String jsonResponse = "Nothing";
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-
+                System.out.println("\n\n" + jsonResponse +"\n");
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                System.out.println( response.body().string());
+                System.out.println("\n\n");
+
+                String bodyString = response.body().string();
+                System.out.println(bodyString);
+                JSONObject json;
+
+                try {
+                    json = new JSONObject(bodyString);
+                } catch (JSONException e) {
+                    json = null;
+
+                }
+                //List<String> list = new ArrayList<String>();
+                JSONObject innerObj;
+                try {
+                    innerObj = json.getJSONObject("reputation_details");
+                } catch (JSONException e) {
+                    innerObj = null;
+                }
+                String riskType;
+                System.out.println(innerObj);
+                if (innerObj != null) {
+                    try {
+                        riskType = innerObj.getString("type");
+
+                    }
+                    catch(JSONException e) {
+                        riskType ="It didnt work";
+
+                    }
+                    System.out.println(riskType);
+                }
+                else {
+                    System.out.println("innerObj was null\n");
+                }
             }
+
+
+
+
+
         });
     }
 
---
+++ app/src/main/java/com/example/andrewjohnson/scambusterz/ResponseMapping.java
@@ -0,0 +1,29 @@
+//package com.example.andrewjohnson.scambusterz;
+//
+//import java.util.List;
+//
+//public class ResponseMapping {
+//    //to implement
+//        // implementation 'com.google.code.gson:gson:2.8.2'
+//
+//    // {"id":"Phone.7ae76fef-a2e2-4b08-cfe3-bc7128b4a708",
+//    // "phone_number":"9991110003",
+//    // "country_code":"US",
+//    // "country_calling_code":"1",
+//    // "reputation_level":3,
+//    // "reputation_details": {
+//    //      "score":75,"type":"RiskType",
+//    //      "category":"ScamOrFraud"
+//    //   },
+//    // "volume_score":1,
+//    // "report_count":0,
+//    // "error":null,
+//    // "warnings":[]}
+//    private String id;
+//    private String phone_number;
+//    private String country_code;
+//    private String country_calling_code;
+//    private String reputation_level;
+//    private List<String> reputation_details
+//
+//}
