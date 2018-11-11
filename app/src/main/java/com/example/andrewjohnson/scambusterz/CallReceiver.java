b8d46f2880b1698a9078c40beb553158079cf8f6
Katharine Russell <katharinerussell@Katharines-MacBook-Pro.local>
2018-11-10 20:51:20 -0500

    Detached head

--- app/src/main/java/com/example/andrewjohnson/scambusterz/CallReceiver.java
+++ app/src/main/java/com/example/andrewjohnson/scambusterz/CallReceiver.java
@@ -4,23 +4,52 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.telephony.TelephonyManager;
+import android.widget.Toast;
+import android.os.Bundle;
+
 
 public class CallReceiver extends BroadcastReceiver{
 
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)) {
-            String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-            if (spamcall(incomingNumber))
-                sendPushNotification(incomingNumber);
-        }
+
+    public void onReceive(Context context, Intent intent){
+
     }
+
+//    private static int lastState = TelephonyManager.CALL_STATE_IDLE;
+//
+//
+//
+//    public CallReceiver(){
+//        super();
+//    }
+//
+//    public void onReceive(Context context, Intent intent) {
+//        String stateStr = intent.getExtras().getString(TelephonyManager.EXTRA_STATE);
+//        String number = intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+//
+//        try {
+//            System.out.println("Receiver Start");
+//            Toast.makeText(context," Receiver start ",Toast.LENGTH_SHORT).show();
+//
+//        }
+//        catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+//            this.incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+//            if (spamcall(incomingNumber))
+//                sendPushNotification(incomingNumber);
+//        }
+//}
+
 
     private Boolean spamcall(String phoneNum){
         return true;
     }
+
     private void sendPushNotification(String incnu){
-        System.out.println(incnu);
+        System.out.println("hello");
     }
+
 }
--- app/src/main/java/com/example/andrewjohnson/scambusterz/MainActivity.java
+++ app/src/main/java/com/example/andrewjohnson/scambusterz/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.andrewjohnson.scambusterz;
 
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.content.Context;
+import android.content.Intent;
+import android.telecom.Call;
+import android.util.Log;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        try{
+            String imei = tm.getDeviceId();
+            System.out.println(imei);
+        }
+        catch(Exception e){
+            System.out.println("error");
+        }
+
+
+
+//        CallReceiver cRec = new CallReceiver();
+//        String ACTION = "com.example.andrewjohnson.scambusterz.CallReceiver";
+//
+//        IntentFilter intentFilter = new IntentFilter(ACTION);
+//        this.registerReceiver(cRec, intentFilter);
     }
 }
