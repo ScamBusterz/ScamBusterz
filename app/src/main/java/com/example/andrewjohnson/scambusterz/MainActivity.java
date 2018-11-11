aad17d796bbd1c7e6740cafa82754e2e5dae9dd4
Daniel Visca <danielvisca96@gmail.com>
2018-11-10 19:05:50 -0500

    Not working Notification, the button does nothing

--- app/src/main/java/com/example/andrewjohnson/scambusterz/MainActivity.java
+++ app/src/main/java/com/example/andrewjohnson/scambusterz/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.andrewjohnson.scambusterz;
 
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private final String CHANNEL_ID = "Spam Caller";
+    private final int NOTIFICATION_ID = 001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Notifications notify = new Notifications();
-        notify.displayNotification();
+    }
+
+    public void displayNotification( View view){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_priority_high_notification);
+        builder.setContentTitle("SPAM CALLER");
+        builder.setContentText("This phone number has a high likelihood of being spam");
+        builder.setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
     }
 }
